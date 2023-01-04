d1e5e441761aca3b5e17193581416d8610ecc721
unknown <likeemnow@gmail.com>
2023-01-04 21:45:21 +0800

    atm.js replaced with async

--- js/atm.js
+++ js/atm.js
@@ -1,36 +1,39 @@
 let submitBtn = document.getElementById("submitBtn");
 let wdrawEl = document.getElementById("withdrawbtn");
-let memberFunc = () => {
+let loggedIn;
+let memberFunc = async () => {
   let pass2 = document.getElementById("password").value;
   let url = `http://localhost:3000/members?passwordUser=${pass2}`;
-  fetch(url)
-    .then((res) => res.json())
-    .then((json) => {
-      let pass = document.getElementById("password").value;
-      let accnum = document.getElementById("username").value;
-      let parseduser = JSON.parse(`{"AccountNum":"${accnum}" }`);
-      let parsedpass = JSON.parse(`{"passwordUser":"${pass}" }`);
+  const res = await fetch(url);
+  const data = await res.json();
 
-      if (
-        parseduser.AccountNum == json[0].AccountNum &&
-        parsedpass.passwordUser == json[0].passwordUser
-      ) {
-        let loggedIn = {
-          AccountName: json[0].AccountName,
-          passwordUser: json[0].passwordUser,
-          AccountNum: json[0].AccountNum,
-          Balance: json[0].Balance,
-          id: json[0].id,
-        };
-        fetch("http://localhost:3000/loggedin", {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify(loggedIn),
-        });
-        window.location.href = "menu.html";
-      }
-    })
-    .catch(() => {
-      alert("Invalid Credentials");
-    });
+  let pass = document.getElementById("password").value;
+  let accnum = document.getElementById("username").value;
+  let parseduser = JSON.parse(`{"AccountNum":"${accnum}" }`);
+  let parsedpass = JSON.parse(`{"passwordUser":"${pass}" }`);
+
+  if (
+    parseduser.AccountNum == data[0].AccountNum &&
+    parsedpass.passwordUser == data[0].passwordUser
+  ) {
+    loggedIn = {
+      AccountName: data[0].AccountName,
+      passwordUser: data[0].passwordUser,
+      AccountNum: data[0].AccountNum,
+      Balance: data[0].Balance,
+      id: data[0].id,
+    };
+    postUserlogin();
+
+  }
+
 };
+
+let postUserlogin = async () => {
+  await fetch("http://localhost:3000/loggedin", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(loggedIn),
+  });
+  window.location.href = "menu.html";
+}
