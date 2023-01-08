d3deebd1fd3ec451abe6b326f6e8fe3c6d9c798a
unknown <likeemnow@gmail.com>
2023-01-08 18:07:50 +0800

    Atm Final Project

--- js/atm.js
+++ js/atm.js
@@ -1,32 +1,35 @@
 let submitBtn = document.getElementById("submitBtn");
 let wdrawEl = document.getElementById("withdrawbtn");
 let loggedIn;
+
 let memberFunc = async () => {
-  let pass2 = document.getElementById("password").value;
-  let url = `http://localhost:3000/members?passwordUser=${pass2}`;
-  const res = await fetch(url);
-  const data = await res.json();
+  try {
+    let pass2 = document.getElementById("password").value;
+    let url = `http://localhost:3000/members?passwordUser=${pass2}`;
+    const res = await fetch(url);
+    const data = await res.json();
 
-  let pass = document.getElementById("password").value;
-  let accnum = document.getElementById("username").value;
-  let parseduser = JSON.parse(`{"AccountNum":"${accnum}" }`);
-  let parsedpass = JSON.parse(`{"passwordUser":"${pass}" }`);
+    let pass = document.getElementById("password").value;
+    let accnum = document.getElementById("username").value;
+    let parseduser = JSON.parse(`{"AccountNum":"${accnum}" }`);
+    let parsedpass = JSON.parse(`{"passwordUser":"${pass}" }`);
 
-  if (
-    parseduser.AccountNum == data[0].AccountNum &&
-    parsedpass.passwordUser == data[0].passwordUser
-  ) {
-    loggedIn = {
-      AccountName: data[0].AccountName,
-      passwordUser: data[0].passwordUser,
-      AccountNum: data[0].AccountNum,
-      Balance: data[0].Balance,
-      id: data[0].id,
-    };
-    postUserlogin();
-
+    if (
+      parseduser.AccountNum == data[0].AccountNum &&
+      parsedpass.passwordUser == data[0].passwordUser
+    ) {
+      loggedIn = {
+        AccountName: data[0].AccountName,
+        passwordUser: data[0].passwordUser,
+        AccountNum: data[0].AccountNum,
+        Balance: data[0].Balance,
+        id: data[0].id,
+      };
+      postUserlogin();
+    }
+  } catch (error) {
+    alert("Invalid Credentials");
   }
-
 };
 
 let postUserlogin = async () => {
@@ -36,4 +39,4 @@ let postUserlogin = async () => {
     body: JSON.stringify(loggedIn),
   });
   window.location.href = "menu.html";
-}
+};
